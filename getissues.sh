9e2a23bb097ddcaca71b9229e1cdf544dfaa34f7
brandon-m-young <b@ufundit.com>
2021-02-21 17:26:20 -0500

    Get issues

---
+++ getissues.sh
@@ -0,0 +1,6 @@
+curl --request GET \
+  --url 'https://ufundit.atlassian.net/rest/api/3/issue/picker?query=' \
+  --user 'brandon.m.young@gmail.com:zBGico821t5Ot3Us7A448998' \
+  --header 'Accept: application/json' > /home/pi/git/jiraconn-service/jira-issues.json
+
+
