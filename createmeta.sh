97b12a4e97cc1d1c0bb738b1e305df8f9f9ae9c6
brandon-m-young <b@ufundit.com>
2021-02-26 17:23:41 -0500

    Removed create issue functionality

--- createmeta.sh
+++
@@ -1,7 +0,0 @@
-curl -D- \
-   -u brandon.m.young@gmail.com:zBGico821t5Ot3Us7A448998 \
-   -X GET \
-   -H "Content-Type: application/json" \
-   https://ufundit.atlassian.net/rest/api/latest/project
-
-
